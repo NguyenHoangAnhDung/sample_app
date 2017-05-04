e29718d1f1f878794593ed3831e21355cad09363
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2017-05-04 08:47:30 +0700

    sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
+  include UsersHelper
   before_save {email.downcase!}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 51}
-  validates :email, format: {with: VALID_EMAIL_REGEX, message: "Only map this regex"},
+  validates :email, format: {with: Settings.VALID_EMAIL_REGEX, message: "Only map this regex"},
     uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 3}
-  validates :password_confirmation, presence: true
   has_secure_password
 end
