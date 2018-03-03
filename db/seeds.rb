0675374c217e00fa7bcbb6e10699db23b51f9298
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2018-03-03 21:37:10 +0700

    [Issue 04] Add authenticate by Devise-Token-Auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Item.delete_all
-
 10.times.each do |i|
   Item.create! name: "Item number #{i}", price: (i + 1) * 10
 end
+
+User.delete_all
+User.create(email: 'user@gmail.com', nickname: 'UOne', name: 'User One', password: "password123")
